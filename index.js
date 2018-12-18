988ff849d16a5fe86b07041ca3447bcab06c83d7
Ashley Achibald <aarchibald20@gmail.com>
2018-12-18 17:57:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
-function sayHiGrandma(string){
+function sayHiToGrandma(string){
   if(string.LowerCase())
   return "I can't hear you !"
+}
+function sayHiToGrandma(string){
+  if (string.UpperCase())
+  return " YES INDEED!"
 }
