63a3c4ea4b33013d5dc386ee6e3c8e0e8facc138
Ashley Achibald <aarchibald20@gmail.com>
2018-12-18 18:27:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,9 +12,12 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   if(string === string.toLowerCase()){
-    return 'I can\nt hear you!';
+    return "I can't hear you!";
+  } else if(string === string.toUpperCase()){
+    return "YES INDEED !"
+  } else{
+    return "I love you, too"
   }
 }
-function sayHiToGrandma(string){
 
 }
